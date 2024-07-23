63a947af8f2b1e0d5c2aea08038cd45b85cbd771
Dinodino1231 <19103291@usc.edu.ph>
2024-07-23 17:54:48 +0800

    using recursion on bubble sort

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,3 +1,9 @@
 
-        printf("%d, ", arr[i]);
-    }
+    //     for(int j = 0; j < size - i - 1; j++){
+    //         if(arr[j] > arr[j+1]){
+    //             int temp = arr[j];
+    //             arr[j] = arr[j+1];
+    //             arr[j+1] = temp;
+    //         }
+    //     }
+    // }
